1766f54da27b9a64fc4ef54f7bf0d32bb4ef2cc1
ImjeekxGurjar <jitender.rawat@techx-consulting.com>
2025-05-05 16:36:34 +0530

    services css and js updated

--- blocks/services/services.js
+++ blocks/services/services.js
@@ -1,60 +1,57 @@
-import { createOptimizedPicture } from "../../scripts/aem.js";
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
-const loadSwiper = () =>
-  new Promise((resolve) => {
-    if (window.Swiper) {
-      resolve(window.Swiper);
-    } else {
-      const swiperCss = document.createElement("link");
-      swiperCss.rel = "stylesheet";
-      swiperCss.href =
-        "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css";
-      document.head.appendChild(swiperCss);
+const loadSwiper = () => new Promise((resolve) => {
+  if (window.Swiper) {
+    resolve(window.Swiper);
+  } else {
+    const swiperCss = document.createElement('link');
+    swiperCss.rel = 'stylesheet';
+    swiperCss.href = 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css';
+    document.head.appendChild(swiperCss);
 
-      const swiperScript = document.createElement("script");
-      swiperScript.src =
-        "https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js";
-      swiperScript.onload = () => resolve(window.Swiper);
-      document.head.appendChild(swiperScript);
-    }
-  });
+    const swiperScript = document.createElement('script');
+    swiperScript.src = 'https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js';
+    swiperScript.onload = () => resolve(window.Swiper);
+    document.head.appendChild(swiperScript);
+  }
+});
 
 export default async function decorate(block) {
   const mainContainer = block.parentElement.parentElement;
 
   const defaultContainer = mainContainer.querySelector(
-    ".default-content-wrapper"
+    '.default-content-wrapper',
   );
 
   // Generate unique identifiers for each slider instance
   const uniqueId = `swiper-${Math.random().toString(36).substr(2, 9)}`;
 
   // Slider Elements
-  const sliderContainer = document.createElement("div");
-  sliderContainer.classList.add("swiper-container", uniqueId);
+  const sliderContainer = document.createElement('div');
+  sliderContainer.classList.add('swiper-container', uniqueId);
 
-  const sliderWrapper = document.createElement("div");
-  sliderWrapper.classList.add("swiper-wrapper");
+  const sliderWrapper = document.createElement('div');
+  sliderWrapper.classList.add('swiper-wrapper');
 
   [...block.children].forEach((row) => {
-    const slide = document.createElement("div");
-    slide.classList.add("swiper-slide");
+    const slide = document.createElement('div');
+    slide.classList.add('swiper-slide');
 
     while (row.firstElementChild) slide.append(row.firstElementChild);
 
     [...slide.children].forEach((div) => {
-      if (div.querySelector("picture")) div.className = "awards-card-image";
-      else div.className = "awards-card-body";
+      if (div.querySelector('picture')) div.className = 'awards-card-image';
+      else div.className = 'awards-card-body';
     });
 
     sliderWrapper.append(slide);
   });
 
-  sliderWrapper.querySelectorAll("img").forEach((img) => {
-    const picture = img.closest("picture");
+  sliderWrapper.querySelectorAll('img').forEach((img) => {
+    const picture = img.closest('picture');
     if (picture) {
       picture.replaceWith(
-        createOptimizedPicture(img.src, img.alt, false, [{ width: "750" }])
+        createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }]),
       );
     }
   });
@@ -62,29 +59,29 @@ export default async function decorate(block) {
   sliderContainer.append(sliderWrapper);
 
   // Button Elements with both unique and swiper-specific classes
-  const buttonContainer = document.createElement("div");
-  buttonContainer.classList.add("button-container-navigation");
+  const buttonContainer = document.createElement('div');
+  buttonContainer.classList.add('button-container-navigation');
 
-  const prevButton = document.createElement("div");
-  prevButton.classList.add("swiper-button-prev", `${uniqueId}-prev`);
+  const prevButton = document.createElement('div');
+  prevButton.classList.add('swiper-button-prev', `${uniqueId}-prev`);
 
-  const nextButton = document.createElement("div");
-  nextButton.classList.add("swiper-button-next", `${uniqueId}-next`);
+  const nextButton = document.createElement('div');
+  nextButton.classList.add('swiper-button-next', `${uniqueId}-next`);
 
   buttonContainer.append(prevButton, nextButton);
 
-  const heading = defaultContainer.querySelector("h1, h2, h3, h4, h5, h6");
+  const heading = defaultContainer.querySelector('h1, h2, h3, h4, h5, h6');
   if (heading) {
-    const headerContainer = document.createElement("div");
-    headerContainer.classList.add("header-container");
-    const headingWrapper = document.createElement("div");
-    headingWrapper.classList.add("heading-wrapper");
+    const headerContainer = document.createElement('div');
+    headerContainer.classList.add('header-container');
+    const headingWrapper = document.createElement('div');
+    headingWrapper.classList.add('heading-wrapper');
 
     headingWrapper.append(heading);
     headerContainer.appendChild(headingWrapper);
 
     headerContainer.appendChild(buttonContainer);
-    if (mainContainer.classList.contains("award-list-home-v2")) {
+    if (mainContainer.classList.contains('award-list-home-v2')) {
       headingWrapper.append(defaultContainer);
     }
     mainContainer.prepend(headerContainer);
@@ -96,10 +93,10 @@ export default async function decorate(block) {
 
   // Ensure buttons exist before initializing Swiper
   if (!prevButton || !nextButton) {
-    console.error("Swiper navigation buttons are missing.");
+    console.error('Swiper navigation buttons are missing.');
     return;
   }
-  if (!mainContainer.classList.contains("award-list-home-v2")) {
+  if (!mainContainer.classList.contains('award-list-home-v2')) {
     // Initialize Swiper using both unique and default classes with infinite scrolling
     setTimeout(() => {
       const swiper = new Swiper(`.${uniqueId}`, {
@@ -149,11 +146,11 @@ export default async function decorate(block) {
   }
   setTimeout(() => {
     const allSlider = mainContainer.querySelectorAll(
-      ".swiper-wrapper .swiper-slide"
+      '.swiper-wrapper .swiper-slide',
     );
     allSlider.forEach((elem) => {
       const widthSlide = elem.style.width;
-      elem.style.setProperty("--slide-width", widthSlide);
+      elem.style.setProperty('--slide-width', widthSlide);
     });
   }, 200);
 }
